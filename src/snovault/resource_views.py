0831c2e92502d682768287c7e1f9c02adbc2ff46
caseylitton <caseylitton@gmail.com>
2019-08-23 13:34:42 -0700

    print indexing embed for 1

--- src/snovault/resource_views.py
+++ src/snovault/resource_views.py
@@ -180,8 +180,7 @@ def uuid_to_path(request, obj, path):
              name='object')
 def item_view_object(context, request):
     print('$'*10, 'resoure_views.py:item_view_object start')
-    print(requrest)
-    figureing out the url here
+    print(dir(request))
     for key, value in request.params.iteritems():
         print(key, value)
     """ Render json structure
