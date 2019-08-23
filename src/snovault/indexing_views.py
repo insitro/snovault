20372da9bff0bc9db53816b174874e480e151770
caseylitton <caseylitton@gmail.com>
2019-08-23 15:57:00 -0700

    print indexing embed for 1

--- src/snovault/indexing_views.py
+++ src/snovault/indexing_views.py
@@ -16,8 +16,9 @@ def includeme(config):
 
 @view_config(context=Item, name='index-data', permission='index', request_method='GET')
 def item_index_data(context, request):
+    path = resource_path(context)
     print('')
-    print('indexing_views.py:item_index_data', '@@index-data start', request.url)
+    print('indexing_views.py:item_index_data', '@@index-data start', path)
     start_time = time.time()
     uuid = str(context.uuid)
     properties = context.upgrade_properties()
@@ -41,7 +42,6 @@ def item_index_data(context, request):
         ]
 
     start_time = time.time()
-    path = resource_path(context)
     paths = {path}
     collection = context.collection
 
@@ -64,7 +64,7 @@ def item_index_data(context, request):
     start_time = time.time()
     path = path + '/'
     # embedded endpoint is in resource_views.py:item_view_embedded
-    print('indexing_views.py:item_index_data', '@@index-data start', path)
+    print('indexing_views.py:item_index_data', 'call embed', path)
     embedded = request.embed(path, '@@embedded')
     print('indexing_views.py:item_index_data', '@@embedded %s %.6f' % (path, time.time() - start_time))
     asdf
