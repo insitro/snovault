131db1d830f88dbb6c23b7e01c915283a7ee129e
caseylitton <caseylitton@gmail.com>
2019-08-23 17:22:54 -0700

    print indexing embed for 1

--- src/snovault/embed.py
+++ src/snovault/embed.py
@@ -91,6 +91,8 @@ def _embed(request, path, as_user='EMBED'):
         sub_start_time = time.time()
         print('embed.py:_embed', 'call invoke_subrequest', path)
         result = request.invoke_subrequest(subreq)
+        if path == '/xlabs/peggy-farnham/':
+            fjfj
         print('embed.py:_embed', 'call invoke_subrequest', path, '%.6f' % (time.time() - sub_start_time))
     except HTTPNotFound:
         raise KeyError(path)
